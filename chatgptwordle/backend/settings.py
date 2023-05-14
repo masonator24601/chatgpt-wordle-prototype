0b2c535a6b271ae450ce2413d1dd8d394020d7e8
Jack Mason <masonjackaws@gmail.com>
2023-05-14 16:25:42 +0100

    update settings

--- chatgptwordle/backend/settings.py
+++ chatgptwordle/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@l(3!)zq0izo#^nmjy$+#3_*y6n637jlssb9@+pyfjya@i_*mp'
+SECRET_KEY = '<SECRET>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
